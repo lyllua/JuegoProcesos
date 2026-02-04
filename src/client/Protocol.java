4a8c2765a0e8d6204621d4f3c49f1c2b725728f6
ly <lydiaserranobellon@gmail.com>
2026-02-04 13:10:43 +0100

    Initial implementation of client logic and game manager

---
+++ src/client/Protocol.java
@@ -0,0 +1,12 @@
+package client;
+
+public class Protocol {
+    public static final String start_command = "NEW_GAME";
+    public static final String game_type = "DICE";
+
+    public static final String end_command = "FINISH";
+
+    public static final String victory = "V";
+    public static final String defeat = "D";
+    public static final String draw = "E";
+}
